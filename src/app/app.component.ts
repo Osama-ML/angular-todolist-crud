3f43e6f4b93787b57dfd445a868e8e7c2eb77249
blueC4P <osamag4g@gmail.com>
2020-10-15 01:18:00 +0100

    create new scripts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,40 @@
 import { Component } from '@angular/core';
-import {TasksService} from './services/tasks.service'
+import { TasksService } from './services/tasks.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
   title = 'crudPrueba';
-  constructor(private tasksService:TasksService){
+  constructor(private tasksService: TasksService) {
     this.showTasks();
   }
 
   public tasks: any[] = [];
   public task = {
-    title: ""
-  }
+    title: '',
+  };
 
-  async showTasks(){
+  async showTasks() {
     this.tasks = await this.tasksService.getTasks();
   }
-  async editTask(task){
+
+  async editTask(task) {
     await this.tasksService.editTask(task);
   }
-  async deleteTask(task){
+
+  async deleteTask(task) {
     await this.tasksService.deleteTask(task);
-    this.tasks = this.tasks.filter(t => t.id != task.id)
+    this.tasks = this.tasks.filter((t) => t.id != task.id);
   }
-  async addTask(task){
-    let taskCreated = await this.tasksService.addNewTask(task)
+
+  async addTask(task) {
+    let taskCreated = await this.tasksService.addNewTask(task);
     this.tasks.push(taskCreated);
+    this.task.title = "";
   }
+
 }
